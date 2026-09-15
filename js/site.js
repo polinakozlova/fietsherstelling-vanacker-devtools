1b6013a09823fd6f1a5a140da182f21f457fffa4
polinakozlova <polina.kozlova127@gmail.com>
2026-09-15 17:54:22 +0200

    Oefensite Atelier Van Damme voor devtools-toets

---
+++ js/site.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+   localStorage.setItem('atelier_laatst_bezocht', window.location.pathname.split('/').pop() || 'index.html');
+
+   if (document.cookie.indexOf('atelier_cookies=') !== -1) {
+      return;
+   }
+
+   const banner = document.createElement('div');
+   banner.className = 'cookiebanner';
+   banner.innerHTML
+      = '<p class="cookiebanner__text">We gebruiken cookies om deze website goed te laten werken.</p>'
+      + '<div class="cookiebanner__actions">'
+      + '<button class="cookiebanner__button" type="button" data-cookies="functioneel">Enkel functionele cookies</button>'
+      + '<button class="cookiebanner__button cookiebanner__button--accent" type="button" data-cookies="alle">Alle cookies toestaan</button>'
+      + '</div>';
+   document.body.append(banner);
+
+   banner.querySelectorAll('.cookiebanner__button').forEach(function(button) {
+      button.addEventListener('click', function() {
+         document.cookie = 'atelier_cookies=' + button.dataset.cookies + '; path=/; max-age=31536000';
+         banner.remove();
+      });
+   });
+});
